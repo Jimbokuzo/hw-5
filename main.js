7968ef33a9a608195fce2524dede63970fed79c6
grunsky yegor <chopgruny@gmail.com>
2022-02-15 19:11:16 +0200

    574

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 "use strict"
-var res = ["Hello world", "p"]
-var result = "<" + res[1] + ">" + res[0] +"</" + res[1] + ">"
-console.log(result) 
-
-
+function wrapTags(text, p){
+    return `<${p}>${text}</${p}>`
+}
+var res = wrapTags("Hello world", "p")
+console.log(res)
